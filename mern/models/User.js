c760b161cb00ba86af7aec3ee927304c51d2a8a7
romkravets <romann.kravets@gmail.com>
2020-10-31 21:46:33 +0200

    error

--- mern/models/User.js
+++ mern/models/User.js
@@ -1,9 +1,9 @@
-const {Schema, model} = require('mongoose')
+const {Schema, model, Types} = require('mongoose')
 
 const shema = new Schema({
    email: {type: String, required: true, unique: true},
    password: {type: String, required: true},
-   links: [type: Types.ObjeciId, ref:'Link']
+   links: [{ type: Types.ObjectId, ref: 'Link' }]
 
 })
 
