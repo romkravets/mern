5ac9d35c22f6f769dd20d7aa6ac79c1ca9e66726
romkravets <romann.kravets@gmail.com>
2020-11-14 11:51:10 +0200

    page DetailLink

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React,{useState,useEffect, useCallback, useContext} from 'react';
+import {useParams} from 'react-router-dom'
+import { useHttp } from './../hooks/http.hooks';
+import { AuthContext } from './../context/AuthContext';
+import { Loader } from './../components/Loader';
+import { LinkCard } from './../components/LinkCard';
 
 export const DetailPage = () => {
-   return (
-      <div>
-         <h1>Detail page</h1>
-      </div>
+   const {token} = useContext(AuthContext);
+   const {request, loading} = useHttp();
+   const [link, setLink] = useState(null);
+   const linkId = useParams().id;
+
+   const getLink = useCallback(async() => {
+      try {
+         const fetched = await request(`/api/link/${linkId}`, 'GET', null, {
+            Authorization: `Bearer ${token}`
+         })
+         console.log(fetched);
+         setLink(fetched);
+      } catch (e) {}
+   }, [token, linkId, request]);
+
+   useEffect(()=> {
+      getLink()
+   }, [getLink])
+
+   if (loading) {
+      return <Loader/>
+   }
+
+    return (
+      <>
+         {!loading && link  && <LinkCard link={link}/>} 
+      </>
    )
 }
